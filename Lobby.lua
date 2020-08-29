cc7c4087106410be4f48aae833232aeb487a7b5e
Sunset <sunrisebestrise@gmail.com>
2020-08-29 15:45:12 +0200

    added clickable buttons

--- Lobby.lua
+++ Lobby.lua
@@ -2,6 +2,7 @@ Lobby = Object:extend()
 
 function Lobby:new()
 	self.buttons = {"Host a game", "Join a game"}
+	screen = "lobby"
 end
 
 function Lobby:draw()
@@ -9,5 +10,9 @@ function Lobby:draw()
 end
 
 function Lobby:update()
-
+	for i=1,#self.buttons do 
+		if suit.Button(self.buttons[i], 75 + (i-1) * 350, 100, 300,70).hit then
+			love.audio.play(sound)
+		end
+	end
 end
--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 Object = require "libs/classic"
 sock = require "libs/sock"
 bitser = require "libs/bitser"
+suit = require "suit"
 
 require "Game"
 require "Localgame"
@@ -10,10 +11,12 @@ require "checkWin"
 require "super"
 
 function love.load()
-	love.graphics.setBackgroundColor(0.5, 0.5, 0.5)
+	love.graphics.setBackgroundColor(0.9, 0.9, 0.9)
 	winFont = love.graphics.newFont("assets/fonts/monogram_extended.ttf", 50)
+	love.graphics.setFont(winFont)
 	love.window.setTitle("Vier Op Een Rij")
 	screen = "startmenu"
+	sound = love.audio.newSource("assets/sounds/click.wav", "static")
 end
 
 function love.keypressed(key)
@@ -24,14 +27,20 @@ function love.keypressed(key)
 	elseif key == 'l' then
 		lobby = Lobby() --creates lobby
 	end
+	suit.keypressed(key)
 end
 
-function love.draw()
+function love.textinput(t)
+	suit.textinput(t)
+end
+
+function love.draw(dt)
 	if screen == "localgame" then 
 		localgame:draw()
 	elseif screen == "lobby" then
 		lobby:draw()
 	end
+	suit.draw(dt)
 end
 
 function love.update()
